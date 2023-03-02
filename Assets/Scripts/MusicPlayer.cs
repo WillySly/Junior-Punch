fd463e84fb4ca59ad9bbd82c12b12a537ecd5013
WillySly <willy.m.sly@gmail.com>
2023-03-02 19:07:28 +0400

    created generic singleton class and moved musicplayer as its child

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MusicPlayer : MonoBehaviour
+public class MusicPlayer : Singleton<MusicPlayer>
 {
-    private void Awake()
-    {
-        int numMusicPlayers = FindObjectsOfType<MusicPlayer>().Length;
-        if (numMusicPlayers > 1)
-        {
-            Destroy(gameObject);
-        }else
-        {
-            DontDestroyOnLoad(gameObject);
-        }
-    }
+
 }
